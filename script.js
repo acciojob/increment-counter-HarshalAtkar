5ae1d363e4a524f18fe32f283a3b3b0fdd8b6777
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-09 23:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let button = document.getElementById('incrementBtn');
 button.addEventListener('click', function() {
 			let counter = document.getElementById("counter");
             let count = parseInt(counter.innerText);
-            alert(`${count}`);
+            alert(count);
             counter.innerText = count + 1;
 });
