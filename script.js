b8dacafac8de3e78f4627e4210cf3bc9a2ca83d2
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-09 23:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //your JS code here. If required.
- let a =prompt(counter);
+ function increment() {
+            let counter = document.getElementById("counter");
+            let count = parseInt(counter.innerText);
+            alert(`The un-incremented value is ${count}`);
+            counter.innerText = count + 1;
+ }    
