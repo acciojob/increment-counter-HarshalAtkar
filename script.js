d8bc1ab05c2bb66331c79cc9f5bdbdc6a9c8ba04
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-09 23:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-//your JS code here. If required.
-// let button = document.getElementById('incrementBtn');
-// button.addEventListener('click', function increment();)
-
-// function increment() {
-//             let counter = document.getElementById("counter");
-//             let count = parseInt(counter.innerText);
-//             alert(`The un-incremented value is ${count}`);
-//             counter.innerText = count + 1;
-//  }    
+   
 let button = document.getElementById('incrementBtn');
 button.addEventListener('click', function() {
 			let counter = document.getElementById("counter");
             let count = parseInt(counter.innerText);
-            alert(`The un-incremented value is ${count}`);
+            alert(`${count}`);
             counter.innerText = count + 1;
 });
